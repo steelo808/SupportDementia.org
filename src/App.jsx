653b9eec4c15940ca3c6a3d851714a98b2045635
Sterling Newsom <sterlingnewsom808@gmail.com>
2023-12-13 17:58:27 -0500

    working on navbar and link routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Navbar from './components/Navbar'
+import  Navbar  from './components/Navbar'
+import React from 'react';
 
 function App() {
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react"
+import Logo from '../components/Images/SupportDementiaLogo.png';
+import {Link} from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+    <Link to="/"><img src ={Logo} className="logo" /></Link>
+      <ul>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
   );
 }
 
 export default Navbar;
+
+
+
+
+
+
+
